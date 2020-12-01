718dad93066ca2946ceff8afcc1c7c9d2e473bd3
Hugo Montenegro <h@hmontenegro.com>
2020-12-01 20:32:25 +0100

    initial vector implementation. Should work with __add__

--- gitfighters/git_fighters.py
+++ gitfighters/git_fighters.py
@@ -19,7 +19,7 @@ class fightingAD:
             The corresponding derivative of user defined functions(s) 'f' evaluated at point 'x'.
     """
 
-    def __init__(self, value, derivative=1.0):
+    def __init__(self, value, derivative=1.0, name='x'):
         """
         INPUTS
         =======
@@ -53,8 +53,14 @@ class fightingAD:
             derivative (float, optional): Derivative
                 lines are supported.
         """
-        self.val = value
-        self.der = derivative
+
+        self.val = np.array(value)  # list of inputs
+        self.der = np.array(derivative)  # list of derivatives (jacobian 1-row matrix)
+        if not isinstance(name, list):
+            self.name = [name]
+        else:
+            self.name = name
+        
 
     def __str__(self):
         """Returns the string representation of the current fightingAD object.
@@ -232,17 +238,35 @@ class fightingAD:
         >>> print(s)
         AD object with value of 11 and derivative of 2
         """
+
+        # create placeholder arrays
+        values = self.val
+        derivatives = self.der
+        names = list.copy(self.name)
+
         try:
-            return fightingAD(self.val + other.val, self.der + other.der)
-        except AttributeError:
-            try:
-                return fightingAD(self.val + other, self.der)
-            except:
-                raise TypeError(
-                    "unsupported operand type(s) for +: {} and {}".format(
-                        type(self).__name__, type(other).__name__
-                    )
-                )
+            # if variable names are the same, then we add them together
+            for i, name in enumerate(self.name):
+                for j, o_name in enumerate(other.name):
+                    if o_name == name:
+                        # then we add it
+                        numpy.put(values, i, self.val.item(i) + other.val.item(j))
+                        numpy.put(derivatives, i, self.der.item(i) + other.der.item(j))
+
+            # if the other variable has new var names, then we append them
+            for i, o_name in enumerate(other.name):
+                if o_name not in self.name:
+                    values = np.append(values, other.val.item(i))
+                    derivatives = np.append(derivatives, other.der.item(i))
+                    names.append(other.name[i])
+
+            # return a new vector with the placeholder arrays
+            return fightingAD(values, derivatives, names)
+        except AttributeError as e:
+            print(e)
+            return fightingAD(self.val + other, self.der, self.name)  # elementwise addition
+        except Exception as e:
+            print(e)
 
     def __radd__(self, other):
         """Called when the left object does not have the __add__ method implemented.
