54196d11fbf28ce161a3348211d9cf43d60c3998
Golo Feige <golofeige@golos-imac.home>
2020-12-10 21:22:54 +0100

    Look into Jac

--- gitfighters/parse.py
+++ gitfighters/parse.py
@@ -7,7 +7,7 @@ from pylatexenc.latexwalker import LatexWalker
 
 
 def parse_latex(formula):
-    w = LatexWalker(repr(formula))
+    w = LatexWalker(formula)
     (nodelist, pos, len_) = w.get_latex_nodes(pos=0)
 
     l2t = LatexNodes2Text()
@@ -15,5 +15,21 @@ def parse_latex(formula):
     print(temp)
 
 
-parse_latex("$\cos({w}) - \frac{y - 5}{z} + e^{x}$")
+def create_cool_latex_jacobian(x):
+
+    startJac = '$J_{ij} = \frac{\partial f_i}{\partial x_j} = $'
+    startDer = '$\frac{\partial f}{\partial x} = $'
+
+    der = differentiate(x)
+
+    #with open('jabobian.tex','w') as file:
+    #    file.write('\\documentclass{article}\n')
+    #    file.write('\\begin{document}\n') 
+    #    file.write(start+'\n')
+    #    file.write('\\end{document}\n')
+
+x = fightingAD(5)
+create_cool_latex_jacobian(x)
+
+
 
