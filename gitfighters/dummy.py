b1cc610d600ca8b391dade3ebf8f7b84709f1205
Golo Feige <golofeige@golos-imac.home>
2020-12-12 12:10:50 +0100

    Deleted dummy file

---
+++ gitfighters/dummy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from sympy import latex
+
+#v0 = Variable('w', 1)
+#v1 = Variable('x', 5)
+#v2 = Variable('y', 3)
+#v3 = Variable('z', 7)
+
+o0 = latex((v0 / v1) * v2**2 + sin(v3))
+print (o0)
